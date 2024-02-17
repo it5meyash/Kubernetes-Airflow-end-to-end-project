c2d15bb03485d94a2f7f6e1866bbfce6340e3dc6
Yash Mulewa <92930714+it5meyash@users.noreply.github.com>
2024-02-17 12:25:29 +0530

    Update fetch_and_preview.py

--- dags/fetch_and_preview.py
+++ dags/fetch_and_preview.py
@@ -7,21 +7,22 @@ import pandas as pd
 
 
 def get_data(**kwargs):
-    url = "https://github.com/it5meyash/Kubernetes-Airflow-end-to-end-project/blob/master/booking.csv"
+    url = "https://github.com/it5meyash/Kubernetes-Airflow-end-to-end-project/raw/master/booking.csv"
     response = requests.get(url)
 
-    if requests.status_codes == 200:
-        df = pd.read_csv(url,header = None, names=['client_id','booking_date','room_type','hotel_id','booking_cost','currency'])
+    if response.status_code == 200:  # Corrected the status_code check
+        df = pd.read_csv(url, header=None, names=['client_id', 'booking_date', 'room_type', 'hotel_id', 'booking_cost', 'currency'])
 
-        #convert dataframe to json string from xcom
+        # Convert dataframe to json string from xcom
         json_data = df.to_json(orient='records')
 
-        kwargs['ti'].xcom_push(key='data',value=json_data)
+        kwargs['ti'].xcom_push(key='data', value=json_data)
 
     else:
         raise Exception(f'Failed to get data, HTTP status code: {response.status_code}')
-    
-def preview_data(**kwargs):
+
+
+def preview_data(**kwargs):  # Corrected function name from priview to preview
     import pandas as pd
     import json
 
@@ -35,39 +36,38 @@ def preview_data(**kwargs):
     # Create Dataframe from JSON data
     df = pd.DataFrame(output_data)
 
-    #Compute total sales
+    # Compute total sales
     df['Total'] = df['booking_cost'] * df.groupby('hotel_id')['hotel_id'].transform('count')
 
-
-    #df = df.groupby('Category', as_index=False).agg({'Quantity': 'sum', 'Total': 'sum'})
-
-    #sort by booking_cost
+    # Sort by booking_cost
     df = df.sort_values(by='Total', ascending=False)
 
     print(df[['room_type', 'Total']].head(20))
-    
+
+
 default_args = {
     'owner': 'datamasterylab.com',
     'start_date': datetime(2024, 1, 25),
-    'catchup': False
+    'catchup': False,
 }
 
 dag = DAG(
-    'fetch_and_priview',
-    default_args = default_args,
-    schedule=timedelta(days=1)
+    'fetch_and_preview',
+     default_args=default_args,
+     schedule_interval=timedelta(days=1)  # Changed schedule to schedule_interval
 )
 
 get_data_from_url = PythonOperator(
-    task_id='get_data',
-    python_callable=get_data,
-    dag=dag
-)
+     task_id='get_data',
+     python_callable=get_data,
+     dag=dag,
+ )
 
 preview_data_from_url = PythonOperator(
-    task_id ='preview_data',
-    python_callable=preview_data,
-    dag=dag
-)
+     task_id='preview_data',
+     python_callable=preview_data,
+     dag=dag,
+ )
 
 get_data_from_url >> preview_data_from_url
+
